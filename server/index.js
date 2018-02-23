746280446c762d4ec374763ec80ef5fd3df12d6c
Cody Selman <codyleeselman@gmail.com>
2018-02-23 14:26:13 -0500

    Fixed typos from previous commit that prevented express server working.

--- server/index.js
+++ server/index.js
@@ -5,27 +5,24 @@ const axios = require('axios');
 //configuration
 require('dotenv').config();
 const{API_KEY}=process.env;
-console.log(API_KEY);
 
 //creation of stuff
 const serverApp = express();
 const port = process.env.PORT || 5000;
 
-serverApp.get('forecast/:lat,:lon', function(request, response){
+serverApp.get('/forecast/:lat,:lon', function(request, response){
     const url = `https://api.darksky.net/forecast/${API_KEY}/37.8267,-122.4233`;
     axios.get(url)
-        .then(res => {
-            response.json({
-                response.json(res.data);
-            })
+    .then(res => {
+        response.json(res.data);
+    })
+    .catch(err => {
+        response.json({
+            msg: 'Your stuff is broken.'
         })
-        .catch(err => {
-            response.json({
-                msg: 'Your stuff is broken.'
-            })
-        });
+    });
 });
 
 serverApp.listen(port, ()=> {
-    console.log('Now listening on port ${port}');
+    console.log(`Now listening on port ${port}`);
 });
