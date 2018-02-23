e2321166585c6b6de720c40a9033672bbb90b6b4
Cody Selman <codyleeselman@gmail.com>
2018-02-23 13:15:22 -0500

    Created basic HTML layout for weather app in App Component.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ class App extends Component {
   render() {
     return (
       <div>
-        hello
+        <h1>Weather</h1>
+        <form>
+          <label>Latitude:</label>
+          <input step='.0001' type="number"/>
+          <label>Longitude:</label>
+          <input step='.0001' type="number"/>
+          <button type='submit'>Get the Weather</button>
+        </form>
       </div>
     );
   }
