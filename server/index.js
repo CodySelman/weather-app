ac1681294859a5f6ccd49866bf69410025b16165
Cody Selman <codyleeselman@gmail.com>
2018-02-24 17:42:12 -0500

    Added google maps api key to template. Began setting up express call for google maps api.

--- server/index.js
+++ server/index.js
@@ -4,7 +4,8 @@ const axios = require('axios');
 
 //configuration
 require('dotenv').config();
-const{API_KEY}=process.env;
+const{DARKSKY_API_KEY}=process.env;
+const{GOOGLEMAPS_API_KEY} = process.env;
 
 //creation of stuff
 const serverApp = express();
@@ -12,16 +13,29 @@ const port = process.env.PORT || 5000;
 
 serverApp.get('/forecast/:lat,:lon', function(request, response){
     const { lat, lon } = request.params;
-    const url = `https://api.darksky.net/forecast/${API_KEY}/${lat},${lon}`;
+    const url = `https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${lat},${lon}`;
     axios.get(url)
     .then(res => {
         response.status(200).json(res.data);
     })
     .catch(err => {
         response.status(500).json({
-            msg: 'Your stuff is broken.'
+            msg: 'Darksky failed on server index.js'
         })
     });
+});
+
+serverApp.get('./geocode/', function(request, response) {
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=${GOOGLEMAPS_API_KEY}`;
+    axios.get(url)
+        .then(res => {
+            response.status(200).json(res.data);
+        })
+        .catch(err => {
+            response.status(500).json({
+                msg: 'Geocoding Failed.'
+            })
+        });
 });
 
 serverApp.listen(port, ()=> {
