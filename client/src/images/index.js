1ab9c01dfbfa2066c3c9647ef63a9d6ad2d28e8f
Cody Selman <codyleeselman@gmail.com>
2018-02-27 20:25:24 -0500

    Imported remaining weather icons into images/index.js and updated cases accordingly.

--- client/src/images/index.js
+++ client/src/images/index.js
@@ -1,9 +1,37 @@
 import clearDay from './clear-day.png';
+import clearNight from './clear-night.png';
+import cloudy from './cloudy.png';
+import fog from './fog.png';
+import partlyCloudyDay from './partly-cloudy-day.png';
+import partlyCloudyNight from './partly-cloudy-night.png';
+import rain from './rain.png';
+import sleet from './sleet.png';
+import snow from './snow.png';
+import wind from './wind.png';
+
 
 const selection = (name) => {
     switch(name){
         case 'clear-day':
             return clearDay;
+        case 'clear-night':
+            return clearNight;
+        case 'cloudy':
+            return cloudy;
+        case 'fog':
+            return fog;
+        case 'partly-cloudy-day':
+            return partlyCloudyDay;
+        case 'partly-cloudy-night':
+            return partlyCloudyNight;
+        case 'rain':
+            return rain;
+        case 'sleet':
+            return sleet;
+        case 'snow':
+            return snow;
+        case 'wind':
+            return wind;
         default:
             return clearDay;
     }
